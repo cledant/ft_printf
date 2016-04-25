d2dceeb1ddce49789152b1c03af644919a6728db
cledant <cledant@student.42.fr>
2016-04-25 16:11:02 +0200

    S et C ok

--- srcs/ft_wputchar.c
+++ srcs/ft_wputchar.c
@@ -6,7 +6,7 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/24 20:03:42 by cledant           #+#    #+#             */
-/*   Updated: 2016/04/25 14:23:01 by cledant          ###   ########.fr       */
+/*   Updated: 2016/04/25 15:44:28 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,54 @@ static void	ft_size_2(wchar_t c)
 	write(1, &part[0], 1);
 }
 
+static void	ft_print_size_4(wchar_t c, unsigned int *tab)
+{
+	unsigned int	part[4];
+
+	ft_bzero(part, sizeof(int) * 4);
+	part[0] = tab[0] & (unsigned int)c;
+	part[1] = tab[1] & (unsigned int)c;
+	part[1] = part[1] << 2;
+	part[2] = tab[2] & (unsigned int)c;
+	part[2] = part[2] << 4;
+	part[3] = tab[3] & (unsigned int)c;
+	part[3] = part[3] << 6;
+	part[0] = tab[4] | part[0];
+	part[1] = tab[5] | part[1];
+	part[1] = part[1] >> 8;
+	part[2] = tab[6] | part[2];
+	part[2] = part[2] >> 16;
+	part[3] = tab[7] | part[3];
+	part[3] = part[3] >> 24;
+	write(1, &part[3], 1);
+	write(1, &part[2], 1);
+	write(1, &part[1], 1);
+	write(1, &part[0], 1);
+}
+
+static void	ft_size_4(wchar_t c)
+{
+	unsigned int	tab[8];
+
+	tab[0] = 63;
+	tab[1] = 4032;
+	tab[2] = 258048;
+	tab[3] = 1835008;
+	tab[4] = 128;
+	tab[5] = 32768;
+	tab[6] = 8388608;
+	tab[7] = 4026531840;
+	ft_print_size_4(c, tab);
+}
+
 static void	ft_size_3(wchar_t c)
 {
-	unsigned int	tab[6];
-	unsigned int	part[3];
+	unsigned int	tab[8];
+	unsigned int	part[4];
 
-	ft_bzero(part, sizeof(int) * 2);
+	ft_bzero(part, sizeof(int) * 3);
 	tab[0] = 63;
-	tab[1] = 1984;
+	tab[1] = 4032;
 	tab[2] = 61440;
 	tab[3] = 128;
 	tab[4] = 32768;
@@ -70,6 +110,6 @@ void		ft_wputchar(wchar_t c)
 		ft_size_2(c);
 	else if (size == 3)
 		ft_size_3(c);
-//	else if (size == 4)
-//		ft_size_4(c);
+	else if (size == 4)
+		ft_size_4(c);
 }
