f24c63b29d30024410669ac850e5b3388c8c1515
cledant <cledant@student.42.fr>
2016-05-02 11:51:48 +0200

    6 erreur 42fc et 5 moulitest

--- srcs/ft_printf_chain_modifier.c
+++ srcs/ft_printf_chain_modifier.c
@@ -6,7 +6,7 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 19:59:04 by cledant           #+#    #+#             */
-/*   Updated: 2016/05/01 20:22:36 by cledant          ###   ########.fr       */
+/*   Updated: 2016/05/02 11:50:48 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,6 @@ char	*ft_printf_chain_modifier(char *s, long long int val, t_flags *f)
 			f->precision == 1)
 		s = ft_printf_mod_rm_preci(s);
 
-	if ((f->type == 3 || f->type == 4 || f->type == 5) && f->space > 0 &&
-			f->sign == 0 && val >= 0)
-		s = ft_printf_add_front_string(" ", s);
-	else if ((f->type == 3 || f->type == 4 || f->type == 5) && f->sign > 0 &&
-			val >= 0)
-		s = ft_printf_add_front_string("+", s);
-
 	if (f->precision == 1 && f->sharp == 0 && (f->type >= 2 && f->type <= 11))
 		s = ft_printf_mod_precision(s, f->preci_size);
 	if (f->precision == 1 && f->preci_size == 0 && (f->type >= 2
@@ -63,6 +56,13 @@ char	*ft_printf_chain_modifier(char *s, long long int val, t_flags *f)
 		s = ft_printf_add_front_string("0x", s);
 	if (f->precision == 1 && (f->type == 12 || f->type == 0) && f->sharp == 0)
 		s = ft_printf_mod_precision_str_simple(s, f->preci_size);
+
+	if ((f->type == 3 || f->type == 4 || f->type == 5) && f->space > 0 &&
+			f->sign == 0 && val >= 0)
+		s = ft_printf_add_front_string(" ", s);
+	else if ((f->type == 3 || f->type == 4 || f->type == 5) && f->sign > 0 &&
+			val >= 0)
+		s = ft_printf_add_front_string("+", s);
 
 	if (f->pad_zero == 0 && f->pad_space == 0 && (f->type >= 2 &&
 			f->type <= 11))
