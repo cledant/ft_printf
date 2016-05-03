820397d8ff309659eaac7e18e1c2daabb9bad96d
cledant <cledant@student.42.fr>
2016-05-03 19:13:52 +0200

    WIP normalisation

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,69 +6,80 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/19 11:30:42 by cledant           #+#    #+#             */
-/*   Updated: 2016/04/27 16:46:12 by cledant          ###   ########.fr       */
+/*   Updated: 2016/05/03 19:12:49 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_printf(const char* restrict format, ...)
+static void		ft_printf_first(char **cpy_ft, size_t *read_c,
+					size_t *ret_parse)
 {
-	va_list list_arg;
-	size_t	read_char;
+	(*cpy_ft)++;
+	ft_putchar(**cpy_ft);
+	(*cpy_ft)++;
+	(*read_c)++;
+	(*ret_parse) = -1;
+}
+
+static void		ft_printf_second(char **cpy_ft, size_t *read_c)
+{
+	if (*(cpy_ft + 1) != NULL && **(cpy_ft + 1) != '\0' &&
+			ft_isspace(**(cpy_ft + 1)) == 1)
+	{
+		(*cpy_ft)++;
+		while (ft_isspace(**cpy_ft) == 1)
+			(*cpy_ft)++;
+		ft_putchar(**cpy_ft);
+		(*cpy_ft)++;
+		(*read_c)++;
+	}
+	else
+		(*cpy_ft)++;
+}
+
+static void		ft_printf_third(char **cpy_ft, size_t *read_c)
+{
+	ft_putchar(**cpy_ft);
+	(*cpy_ft)++;
+	(*read_c)++;
+}
+
+static void		ft_printf_cases(int skip, char **cpy_ft, size_t *read_c,
+					size_t *ret_parse)
+{
+	if (skip == 1)
+		ft_printf_first(cpy_ft, read_c, ret_parse);
+	if (*ret_parse == 0)
+		ft_printf_second(cpy_ft, read_c);
+	else
+		(*cpy_ft) += (*ret_parse) + 1;
+}
+
+int				ft_printf(const char *restrict format, ...)
+{
+	va_list list;
+	size_t	read_c;
 	size_t	ret_parse;
 	int		skip;
-	char	*cpy_format;
-	char	*cpy_format_2;
+	char	*cpy_ft;
 
-	if ((cpy_format = ft_strdup(format)) == NULL)
-		return 0;
-	cpy_format_2 = cpy_format;
-	read_char = 0;
-	va_start(list_arg, format);
-	while (*cpy_format != '\0')
+	cpy_ft = (char *)format;
+	read_c = 0;
+	va_start(list, format);
+	while (*cpy_ft != '\0')
 	{
 		skip = 0;
-		if (ft_strncmp(cpy_format, "%", 1) == 0)
+		if (ft_strncmp(cpy_ft, "%", 1) == 0)
 		{
-			ret_parse = ft_printf_parse_type(cpy_format, list_arg,
-					&skip, &read_char);
-			if (skip == 1)
-			{
-				cpy_format++;
-				ft_putchar(*cpy_format);
-				cpy_format++;
-				read_char++;
-				ret_parse = -1;
-			}
-			if (ret_parse == 0)
-			{	
-				if(*(cpy_format + 1) != '\0' &&
-						ft_isspace(*(cpy_format + 1)) == 1)
-				{
-					cpy_format++;
-					while(ft_isspace(*cpy_format) == 1)
-						cpy_format++;
-					ft_putchar(*cpy_format);
-					cpy_format++;
-					read_char++;
-				}
-				else
-					cpy_format++;
-			}
-			else
-				cpy_format += ret_parse + 1;
+			ret_parse = ft_printf_parse_type(cpy_ft, list, &skip, &read_c);
+			ft_printf_cases(skip, &cpy_ft, &read_c, &ret_parse);
 		}
 		else
-		{
-			ft_putchar(*cpy_format);
-			cpy_format++;
-			read_char++;
-		}
+			ft_printf_third(&cpy_ft, &read_c);
 		ret_parse = 0;
 	}
 	ft_putchar('\0');
-	ft_strdel(&cpy_format_2);
-	va_end(list_arg);
-	return (read_char);
+	va_end(list);
+	return (read_c);
 }
